157c1371d803f5664eb34b73e75edc4f246c3323
Christian Vargas <cristianvargas385@gmail.com>
2023-07-26 09:02:35 -0500

    header and footer complete

--- src/components/templates/Footer/Footer.jsx
+++ src/components/templates/Footer/Footer.jsx
@@ -1,5 +1,35 @@
 import React from 'react'
 import Logo from '../../atoms/Logo/Logo';
+import './Footer.scss';
+import facebookIcon from '../../../assets/images/icon/facebook-icon.svg'
+import instagramIcon from '../../../assets/images/icon/instagram-icon.svg'
+import twitterIcon from '../../../assets/images/icon/twitter-icon.svg'
+import youtubeIcon from '../../../assets/images/icon/youtube-icon.svg'
+import { SocialMedia } from '../../organisms/SocialMedia/SocialMedia';
+
+const socialMedias = [
+  {
+    title: "facebook",
+    image: facebookIcon,    
+    url: "https://www.facebook.com/SuzukiAutosColombia/"
+  },
+  {
+    title: "instagram",
+    image: instagramIcon,    
+    url: "https://www.instagram.com/suzukiautoscol/"
+  },
+  {
+    title: "twitter",
+    image: twitterIcon,    
+    url: "https://twitter.com/SUZUKIAUTOSCO"
+  },
+  {
+    title: "youtube",
+    image: youtubeIcon,    
+    url: "https://www.youtube.com/suzukiautos"
+  },
+
+]
 
 const contentFooter = [
   {
@@ -27,59 +57,55 @@ const contentFooter = [
   },
 
 ]
-export const Footer = () => {
 
-  const showContent = () => {
-    
-  }
+
+export const Footer = () => {
 
   return (
     <footer className='footer'>
-      <div className='footer--top'>
-        <Logo />
-      </div>
-      <ul>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
+      <div className='footer__content'>
 
-      </ul>
-      
-      {contentFooter.map(content => {
-        console.log(content.title);
-        return (
-          <div className='footer--content' key={content.title}>
-            <h4>{content.title}</h4>
-            <ul>
-              {content.links.map(link => {
-                return <li key={link}>
-                  <a href={"hol"}>{link}</a>
-                </li>
-              })}
-            </ul>
+        <div className='footer--top'>
+          <Logo />
+          <ul className='social'>
+            {socialMedias.map(social => {
+              return <SocialMedia imageData={social} key={social.title}/>
+            })}
+          </ul>
+        </div>
+        
+        <div className='footer--bottom'>
+          {contentFooter.map(content => {
+            console.log(content.title);
+            return (
+              <div className='bottom--content' key={content.title}>
+                <h4 className='bottom--content__title'>{content.title}</h4>
+                <ul className='bottom--content__list'>
+                  {content.links.map(link => {
+                    return <li className='bottom--content__items' key={link}>
+                      <a href={"hol"}>{link}</a>
+                    </li>
+                  })}
+                </ul>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className='footer--copyright'>
+          <div className='copyright'>
+          <ul className='copyright__paragraph'>
+            <li className='copyright__items'>
+              <a href="">Términos y Condiciones <span>|</span></a>
+              <a href="">Política de protección de datos <span>|</span></a>
+              <a href="">Política de Cookies</a>
+            </li>
+          </ul>
+          <div className='footer--copyright__copyright'>
+            <p>© 2022. Todos los derechos reservados - Derco Colombia S.A.S, NIT: 900.327.290-9</p>
           </div>
-        )
-      })}
 
-      <div className='footer--bottom'>
-        <ul className='bottom__paragraph'>
-          <li>
-            <a href="">Términos y Condiciones</a> <span>|</span>
-            <a href="">Política de protección de datos</a> <span>|</span>
-            <a href="">Política de Cookies</a>
-          </li>
-        </ul>
-        <div className='copyright'>
-          <p>© 2022. Todos los derechos reservados - Derco Colombia S.A.S, NIT: 900.327.290-9</p>
+          </div>
         </div>
       </div>
     
